33e4c6519d4b7755db5b5b80048907f160e1aeaf
Nayeon Kim <skdus3373@gmail.com>
2022-10-10 11:12:22 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#define MAX_SIZE 50
+bool visited[MAX_SIZE];
+
+void dfs(int* item, int* bucket, int n, int start, int depth) {
+	if (depth == 6) {
+		for (int i = 0; i < 6; i++)
+			printf("%d ", bucket[i]);
+		printf("\n");
+		return;
+	}
+
+	for (int i = start; i < n; i++) {
+		bucket[depth] = item[i];
+		dfs(item, bucket, n, i + 1, depth + 1);
+	}
+}
+
+int main() {
+	int n;
+	int* item;
+	int bucket[6];
+
+	while (1) {
+		scanf("%d", &n);
+		if (!n) break;
+		item = (int*)malloc(sizeof(int) * n);
+
+		for (int i = 0; i < n; i++) {
+			scanf("%d", &item[i]);
+		}
+		dfs(item, bucket, n, 0, 0);
+		printf("\n");
+
+		free(item);
+	}
+
+	return 0;
+}
